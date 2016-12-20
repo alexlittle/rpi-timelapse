4d6d4b61170f86d2de5d942c30b38906c3344dda
alexlittle <alex@alexlittle.net>
2016-12-21 00:43:45 +0200

    update

--- extract-photos.py
+++ extract-photos.py
@@ -12,23 +12,24 @@ from shutil import copy2
 
 ROOT_DIR = "/home/alex/data/photos/2016/2016-timelapse02-rpi/"
 OUTPUT_DIR = "/home/alex/temp/timelapse/"
-HOURS_TO_EXTRACT = [(9,30), 
-                    (10,00), 
-                    (10,30), 
-                    (11,00), 
-                    (11,30), 
+HOURS_TO_EXTRACT = [#(9,30), 
+                    #(10,00), 
+                    #(10,30), 
+                    #(11,00), 
+                    #(11,30), 
                     (12,00), 
-                    (12,30),
-                    (13,00),
-                    (13,30), 
-                    (14,00),
-                    (14,30), 
-                    (15,00),
-                    (15,30),
-                    (16,00)]
+                    #(12,30),
+                    #(13,00),
+                    #(13,30), 
+                    #(14,00),
+                    #(14,30), 
+                    #(15,00),
+                    #(15,30),
+                    #(16,00)
+					]
 ADD_OVERLAY = True
 FONT = ImageFont.truetype("FreeSansOblique.ttf", 100)
-FRAMERATES = [10,20]
+FRAMERATES = [3]
 
 dirs = sorted(os.listdir(ROOT_DIR))
 
@@ -62,8 +63,9 @@ for d in dirs:
                 photos.append(photo)
 
 for counter, p in enumerate(photos):
-    filename =  "image%04d.jpg" % (counter + 1)
-    print filename
+    
+    filename = "image%04d.jpg" % (counter + 1)
+    print "processing: %s as %s" % (p['filename'], filename )
     copy2(p['path'],os.path.join(OUTPUT_DIR,filename))
     
     if ADD_OVERLAY:
