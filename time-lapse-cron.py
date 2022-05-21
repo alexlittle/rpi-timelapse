7c4785faeaf2caf3f7a54ada5c1867bc2cc60327
Alex Little <consult@alexlittle.net>
2022-05-21 07:49:34 +0300

    update to use libcamera package

--- time-lapse-cron.py
+++ time-lapse-cron.py
@@ -1,16 +1,9 @@
-
 import datetime
 import os
 
-OUTPUT_DIR =  '/home/camera/output'
 
-try:
-    import picamera
-    camera = picamera.PiCamera()
-    camera.resolution = (2592, 1944)
-except picamera.exc.PiCameraMMALError:
-    exit()
- 
+OUTPUT_DIR =  '/home/alex/timelapse/output'
+
 current_time = datetime.datetime.now()
 
 # set the directory to store the captured photo in, and create if doesn't exist
@@ -19,6 +12,6 @@ DAILY_DIR = os.path.join(OUTPUT_DIR, current_time.strftime("%Y-%m-%d"))
 if not os.path.isdir(DAILY_DIR):
     os.makedirs(DAILY_DIR)
 
-# actually capture the photo
-camera.capture(os.path.join(DAILY_DIR,
-                            current_time.strftime("%Y-%m-%d-%H-%M-%S")+'.jpg'))
+out_file = os.path.join(DAILY_DIR, current_time.strftime("%Y-%m-%d-%H-%M-%S")+'.jpg')
+
+os.system('libcamera-jpeg -o %s' % out_file)
